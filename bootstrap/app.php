4b4c504883077ab84ef15c24551c4f588000377e
zacktagnan <zacktagnan@gmail.com>
2025-05-30 08:28:39 +0200

    Control.de.Excepciones-Throttle.y.NotFound

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,36 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use App\Services\API\V1\ApiResponseService;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (ThrottleRequestsException $exception) {
+            $retryAfter = data_get($exception->getHeaders(), 'Retry-After', 60);
+            $maxAttempts = data_get($exception->getHeaders(), 'X-RateLimit-Limit', 60);
+
+            return ApiResponseService::throttled(
+                maxAttempts: $maxAttempts,
+                retryAfter: $retryAfter
+            );
+        });
+
+        $exceptions->render(function (NotFoundHttpException $exception) {
+            return ApiResponseService::notFound(
+                message: $exception->getMessage() ?: 'Resource not found.'
+            );
+        });
     })->create();
