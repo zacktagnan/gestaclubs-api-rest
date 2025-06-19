00ce14d58f1dc916b9f8e0821c8f66cc32f075f3
zacktagnan <zacktagnan@gmail.com>
2025-06-20 01:03:26 +0200

    Test-Manage-ClubCtrller-DetailTest

--- tests/Feature/API/V1/Management/ClubController/ClubTestCase.php
+++ tests/Feature/API/V1/Management/ClubController/ClubTestCase.php
@@ -3,11 +3,13 @@
 namespace Tests\Feature\API\V1\Management\ClubController;
 
 use Tests\TestCase;
+use App\Models\Club;
 
 abstract class ClubTestCase extends TestCase
 {
     protected string $clubsBaseRouteName;
     protected string $table;
+    protected Club $club;
 
     protected function setUp(): void
     {
@@ -15,5 +17,7 @@ abstract class ClubTestCase extends TestCase
 
         $this->clubsBaseRouteName = 'v1.clubs.';
         $this->table = 'clubs';
+
+        $this->club = Club::factory()->create();
     }
 }
---
+++ tests/Feature/API/V1/Management/ClubController/DetailTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Feature\API\V1\Management\ClubController;
+
+use PHPUnit\Framework\Attributes\DataProviderExternal;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\Group;
+use Tests\DataProviders\ClubDataProvider;
+use Tests\Helpers\DTOs\RateLimitTestOptionsDTO;
+use Tests\Helpers\Traits\RateLimitTestHelpers;
+
+#[Group('api:v1')]
+#[Group('api:v1:management')]
+#[Group('api:v1:management:clubs')]
+#[Group('api:v1:management:clubs:detail')]
+class DetailTest extends ClubTestCase
+{
+    use RateLimitTestHelpers;
+
+    #[Test]
+    #[Group('api:v1:management:clubs:detail:success')]
+    public function a_club_can_be_retrieved(): void
+    {
+        $response = $this
+            ->withToken($this->token)
+            ->getJson(route($this->clubsBaseRouteName . 'show', $this->club))
+            ->assertOk();
+
+        $response->assertJson([
+            'data' => [
+                'id' => $this->club->id,
+                'name' => $this->club->name,
+                'budget' => $this->club->budget,
+                'created_at' => $this->club->created_at->format('Y-m-d'),
+            ],
+        ]);
+    }
+
+    #[Test]
+    #[Group('api:v1:management:clubs:detail:not_be_found')]
+    public function a_club_cannot_be_found(): void
+    {
+        $this
+            ->withToken($this->token)
+            ->getJson(route($this->clubsBaseRouteName . 'show', 999))
+            ->assertNotFound();
+    }
+
+    #[Test]
+    #[Group('api:v1:management:clubs:detail:too_many_requests')]
+    public function it_returns_rate_limit_exceeded_when_too_many_requests(): void
+    {
+        $this->assertRateLimitExceeded(new RateLimitTestOptionsDTO(
+            route: route($this->clubsBaseRouteName . 'show', $this->club),
+            method: 'getJson',
+            payload: [],
+            uniqueFields: null,
+            maxAttempts: 10,
+            token: $this->token,
+        ));
+    }
+
+    #[Test]
+    #[Group('api:v1:management:clubs:detail:unauthenticated')]
+    public function an_unauthenticated_user_cannot_retrieve_a_club(): void
+    {
+        $this
+            ->getJson(route($this->clubsBaseRouteName . 'show', $this->club))
+            ->assertUnauthorized();
+    }
+}
