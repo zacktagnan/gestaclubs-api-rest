717386b6f717eea451376d3f443879bc0055576d
zacktagnan <zacktagnan@gmail.com>
2025-06-21 01:36:45 +0200

    ListTest-Considerando.Club.de.ClubTestCase

--- tests/Feature/API/V1/Management/ClubController/ListTest.php
+++ tests/Feature/API/V1/Management/ClubController/ListTest.php
@@ -20,7 +20,7 @@ class ListTest extends ClubTestCase
     #[Group('api:v1:management:clubs:list:success')]
     public function clubs_can_be_listed_by_authenticated_user(): void
     {
-        Club::factory()->count(11)->create();
+        Club::factory()->count(10)->create();
 
         $response = $this
             ->withToken($this->token)
@@ -28,6 +28,7 @@ class ListTest extends ClubTestCase
             ->assertOk();
 
         $this->assertCount(11, $response->json('data'));
+        // 11: el creado desde ClubTestCase y los creados en este test
 
         $response->assertJsonStructure([
             'data' => [
